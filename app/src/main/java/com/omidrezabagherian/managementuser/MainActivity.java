f6f653524e82bce9b495270611e102911ffb5a78
OmidrezaBagherian <OmidCoder21@gmail.com>
2022-07-12 19:42:05 +0430

    Config bottom navigation in project

--- app/src/main/java/com/omidrezabagherian/managementuser/MainActivity.java
+++ app/src/main/java/com/omidrezabagherian/managementuser/MainActivity.java
@@ -1,9 +1,13 @@
 package com.omidrezabagherian.managementuser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationBarView;
 import com.omidrezabagherian.managementuser.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +20,25 @@ public class MainActivity extends AppCompatActivity {
 
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
 
+        initBottomNavigation();
+
         setContentView(mainBinding.getRoot());
+    }
+
+    private void initBottomNavigation(){
+        mainBinding.bottomNavigationViewMain.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.tabHome:
+                        Toast.makeText(MainActivity.this, "home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.tabAdd:
+                        Toast.makeText(MainActivity.this, "add", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
